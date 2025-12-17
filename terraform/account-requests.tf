4b4d297c238ce62c67ed8109d779d4ce8122a2f4
sebasu87 <sebastian.leo87@gmail.com>
2025-12-17 15:40:16 +0800

    Add AFT account request for sandbox account

---
+++ terraform/account-requests.tf
@@ -0,0 +1,35 @@
+module "account_request_01" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "leoseba8787@gmail.com"
+    AccountName               = "ssl-sandbox"
+    ManagedOrganizationalUnit = "Sandbox" 
+    SSOUserEmail              = "leoseba8787+ssl@gmail.com"
+    SSOUserFirstName          = "Suresh"
+    SSOUserLastName           = "Peter"
+  }
+
+  account_tags = {
+    "ABC:Owner"       = "ssl@mycompany.com"
+    "ABC:Division"    = "ENT"
+    "ABC:Environment" = "Dev"
+    "ABC:CostCenter"  = "123456"
+    "ABC:Vended"      = "true"
+    "ABC:DivCode"     = "102"
+    "ABC:BUCode"      = "ABC003"
+    "ABC:Project"     = "123456"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "AWS Control Tower Lab"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform (AFT)"
+  }
+
+  custom_fields = {
+    custom1 = "a"
+    custom2 = "b"
+  }
+
+  account_customizations_name = "SANDBOX"
+}
